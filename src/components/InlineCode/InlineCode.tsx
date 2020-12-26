c0562d62b7821e5e4f1c1cb818e076f48fe7fec6
samrose3 <samrose3@gmail.com>
2020-12-25 20:13:03 -0600

    fix(sytles): update InlineCode

--- src/components/InlineCode/InlineCode.tsx
+++ src/components/InlineCode/InlineCode.tsx
@@ -3,9 +3,9 @@ import styled from 'styled-components';
 export default styled.span`
   display: inline-block;
   font-family: var(--font-family-mono);
-  font-size: 0.9em;
+  font-size: 0.85em;
   letter-spacing: -0.5px;
-  padding: 2px 6px;
+  padding: 0 6px;
   background: var(--color-gray-100);
   border-radius: 0.25rem;
 `;
