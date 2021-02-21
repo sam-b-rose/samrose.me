7b78e48860a6e80ef62b3749bc13998ddd547db2
samrose3 <samrose3@gmail.com>
2021-02-21 13:53:27 -0600

    fix(about): update copy and increase spacing

--- src/components/MainContent/MainContent.tsx
+++ src/components/MainContent/MainContent.tsx
@@ -5,6 +5,7 @@ import { Z_INDICES } from '@constants';
 const MainContent = styled.main`
   position: relative;
   z-index: ${Z_INDICES.mainContent};
+  margin: 3rem 0;
 `;
 
 MainContent.defaultProps = {
