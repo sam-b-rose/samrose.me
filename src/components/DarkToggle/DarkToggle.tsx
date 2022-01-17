9b3f47d8e10ffb168fdf4d46cc32623cb4d2d760
Sam Rose <samrose3@gmail.com>
2022-01-17 11:30:20 -0700

    feat: update dark toggle icon

--- src/components/DarkToggle/DarkToggle.tsx
+++ src/components/DarkToggle/DarkToggle.tsx
@@ -23,66 +23,20 @@ const DarkToggle = () => {
       }}
     >
       <svg
-        width="18"
-        height="18"
-        viewBox="0 0 18 18"
-        style={{ transform: 'rotate(40deg)' }}
+        viewBox="0 0 32 32"
+        width="1em"
+        height="1em"
+        color="var(--colors-text)"
       >
-        <mask id="moon-mask-main-nav">
-          <rect x="0" y="0" width="18" height="18" fill="#FFF"></rect>
-          <circle cx="10" cy="2" r="8" fill="black"></circle>
-        </mask>
         <circle
-          cx="9"
-          cy="9"
-          fill="var(--colors-text)"
-          mask="url(#moon-mask-main-nav)"
-          r="8"
+          cx="16"
+          cy="16"
+          r="14"
+          fill="none"
+          stroke="currentColor"
+          stroke-width="4"
         ></circle>
-        <g>
-          <circle
-            cx="17"
-            cy="9"
-            r="1.5"
-            fill="var(--colors-text)"
-            style={{ transformOrigin: 'center center', transform: 'scale(0)' }}
-          ></circle>
-          <circle
-            cx="13"
-            cy="15.928203230275509"
-            r="1.5"
-            fill="var(--colors-text)"
-            style={{ transformOrigin: 'center center', transform: 'scale(0)' }}
-          ></circle>
-          <circle
-            cx="5.000000000000002"
-            cy="15.92820323027551"
-            r="1.5"
-            fill="var(--colors-text)"
-            style={{ transformOrigin: 'center center', transform: 'scale(0)' }}
-          ></circle>
-          <circle
-            cx="1"
-            cy="9.000000000000002"
-            r="1.5"
-            fill="var(--colors-text)"
-            style={{ transformOrigin: 'center center', transform: 'scale(0)' }}
-          ></circle>
-          <circle
-            cx="4.9999999999999964"
-            cy="2.071796769724492"
-            r="1.5"
-            fill="var(--colors-text)"
-            style={{ transformOrigin: 'center center', transform: 'scale(0)' }}
-          ></circle>
-          <circle
-            cx="13"
-            cy="2.0717967697244912"
-            r="1.5"
-            fill="var(--colors-text)"
-            style={{ transformOrigin: 'center center', transform: 'scale(0)' }}
-          ></circle>
-        </g>
+        <path d=" M 16 0 A 16 16 0 0 0 16 32 z " fill="currentColor"></path>
       </svg>
     </ToggleButton>
   );
@@ -91,6 +45,7 @@ const DarkToggle = () => {
 const ToggleButton = styled(InvisibleButton, {
   display: 'flex',
   alignItems: 'center',
+  margin: '1px 0 0',
 });
 
 export default DarkToggle;
